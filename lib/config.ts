1afc9227a5049b5f32ba3533a6f024bf33d3e15d
quan83852 <quan83852@gmail.com>
2025-12-12 19:56:12 +0700

    update config

--- lib/config.ts
+++ lib/config.ts
@@ -8,7 +8,7 @@ import { getFullnodeUrl } from "@iota/iota-sdk/client"
 import { createNetworkConfig } from "@iota/dapp-kit"
 
 // Package IDs - These will be automatically filled when you run `npm run iota-deploy`
-export const DEVNET_PACKAGE_ID = "0xe7a4eb3db0d780f6ca5525b9e0bf12ba64220d05ef7cf13a8afc330b9cbf9bac"
+export const DEVNET_PACKAGE_ID = "0xb3dc4f8acbf0371ad4808bd1c8952e19e91d00651763b4caabbcdedddbcf62fa"
 export const TESTNET_PACKAGE_ID = ""
 export const MAINNET_PACKAGE_ID = ""
 
